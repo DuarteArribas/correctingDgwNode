b4805609b5200f7508627d48f7ea43b1fcd010e1
DuarteArribas <duarte.a.arribas@gmail.com>
2023-12-20 14:56:31 +0000

    finished the program

--- correctingDgwNode.py
+++ correctingDgwNode.py
@@ -31,41 +31,46 @@ def main():
     config.get("DEFAULT","DGW_DB_PWD")
   )
   dgw.connect()
-  print("Connecting to NOA")
-  noa  = DBConnection(
-    config.get("DEFAULT","NOA_IP"),
-    config.get("DEFAULT","NOA_PORT"),
-    config.get("DEFAULT","NOA_DB_NAME"),
-    config.get("DEFAULT","NOA_DB_USER"),
-    config.get("DEFAULT","NOA_DB_PWD")
+  print(f"Connecting to {config.get('DEFAULT','NODE_NAME')}")
+  node = DBConnection(
+    config.get("DEFAULT","NODE_IP"),
+    config.get("DEFAULT","NODE_PORT"),
+    config.get("DEFAULT","NODE_DB_NAME"),
+    config.get("DEFAULT","NODE_DB_USER"),
+    config.get("DEFAULT","NODE_DB_PWD")
   )
-  noa.connect()
+  node.connect()
   print("--------")
   insertCommands = []
   updateCommands = []
   deleteCommands = []
-  noa.cursor.execute("SELECT id FROM station;")
-  noaStationIds = noa.cursor.fetchall()
-  for noaStationId in noaStationIds:
-    noa.cursor.execute("SELECT * FROM rinex_file WHERE id_station = %s",(noaStationId[0],))
-    noaFiles = noa.cursor.fetchall()
-    dgw.cursor.execute("SELECT * FROM rinex_file WHERE id_station = %s",(noaStationId[0],))
+  deleteCommands2 = []
+  node.cursor.execute("SELECT id FROM station;")
+  nodeStationIds = node.cursor.fetchall()
+  for nodeStationId in nodeStationIds:
+    node.cursor.execute("SELECT * FROM rinex_file WHERE id_station = %s",(nodeStationId[0],))
+    nodeFiles = node.cursor.fetchall()
+    dgw.cursor.execute("SELECT * FROM rinex_file WHERE id_station = %s",(nodeStationId[0],))
     dgwFiles = dgw.cursor.fetchall()
-    for file in noaFiles:
-      if file[13] > 0 and file not in dgwFiles:
-        areFilesDifferentButEqual = [filesDifferentButEqual(file,dgwFile) for dgwFile in dgwFiles]
-        if all(fileDifferent[0] == -1 for fileDifferent in areFilesDifferentButEqual):
-          insertCommands.append(f"INSERT INTO rinex_file VALUES({','.join([item for item in file])});")
-          print("File added for insert")
-        for fileDifferent in areFilesDifferentButEqual:
-          if fileDifferent[0] == 1:
-            updateCommands.append(f"UPDATE rinex_file SET id = {fileDifferent[2][0]} id_station = {fileDifferent[2][2]}, id_data_center_structure = {fileDifferent[2][3]}, file_size = {fileDifferent[2][4]}, id_file_type = {fileDifferent[2][5]}, relative_path = {fileDifferent[2][6]}, reference_date = {fileDifferent[2][7]}, creation_date = {fileDifferent[2][8]}, published_date = {fileDifferent[2][9]}, revision_date = {fileDifferent[2][10]}, md5checksum = {fileDifferent[2][11]}, md5uncompressed = {fileDifferent[2][12]}, status = {fileDifferent[2][13]};")
-            print("File added for update")
-      elif file[13] <= 0 and file in dgwFiles:
+    for file in nodeFiles:
+      if file[13] > 0 and not isIn(file,dgwFiles):
+        insertCommands.append(f"INSERT INTO rinex_file VALUES({','.join([item for item in file])});")
+        print("File added for insert")
+      elif file[13] > 0 and isIn(file,dgwFiles):
+        creationDate = 'NULL' if not file[8] else "'" + str(file[8]) + "'"
+        publishedDate = 'NULL' if not file[9] else "'" + str(file[9]) + "'"
+        revisionDate = 'NULL' if not file[10] else "'" + str(file[10]) + "'"
+        updateCommands.append(f"UPDATE rinex_file SET id_station = {file[2]}, id_data_center_structure = {file[3]}, file_size = {file[4]}, id_file_type = {file[5]}, relative_path = '{file[6]}', reference_date = '{file[7]}', creation_date = {creationDate}, published_date = {publishedDate}, revision_date = {revisionDate}, md5checksum = '{file[11]}', md5uncompressed = '{file[12]}' WHERE md5checksum = '{file[11]}';")
+        print("File added for update")
+      elif file[13] <= 0 and isIn(file,dgwFiles):
         deleteCommands.append(f"DELETE FROM rinex_file WHERE id = {file[0]};")
         print("File added for delete")
+    for file in dgwFiles:
+      if not isIn(file,nodeFiles):
+        deleteCommands2.append(f"DELETE FROM rinex_file WHERE id = {file[0]};")
+        print("File added for delete")
   print("--------")
-  print(f"Finished processing files. {len(insertCommands)} files to be inserted, {len(updateCommands)} files to be updated and {len(deleteCommands)} files to be deleted for a total of {len(insertCommands)+len(updateCommands)+len(deleteCommands)} files to be changed.")
+  print(f"Finished processing files. {len(insertCommands)} files to be inserted, {len(updateCommands)} files to be updated and {len(deleteCommands)} + {len(deleteCommands2)} ({len(deleteCommands) + len(deleteCommands2)}) files to be deleted for a total of {len(insertCommands)+len(updateCommands)+len(deleteCommands)+len(deleteCommands2)} files to be changed.")
   with open("insertCommands.sql","w") as f:
     f.write("BEGIN;\n")
     for command in insertCommands:
@@ -85,18 +90,18 @@ def main():
     for command in deleteCommands:
       f.write(command)
       f.write("\n")
+    for command in deleteCommands2:
+      f.write(command)
+      f.write("\n")
     f.write("COMMIT;")
   print("deleteCommands.sql created")
   print("--------")
   
-def filesDifferentButEqual(file1,file2):
-  if file1[1] == file2[1]: #filename
-    if any([file1[i] != file2[i] for i in range(2,13)]): #all other fields, besides id and status
-      return [1,file1,file2]
-    else:
-      return [0,"",""]
-  else:
-    return [-1,"",""]
+def isIn(file,dgwFiles):
+  for dgwFile in dgwFiles:
+    if file[11] == dgwFile[11]:
+      return True
+  return False
             
 if __name__ == "__main__":
     main()
